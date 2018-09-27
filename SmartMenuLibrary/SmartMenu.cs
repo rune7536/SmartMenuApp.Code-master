a3e46bcc46cfbadca1748d65933ab02b5da69b00
rune7536 <rune7536edu.eal.dk>
2018-09-27 12:06:53 +0200

    streamreader færdig

--- SmartMenuLibrary/SmartMenu.cs
+++ SmartMenuLibrary/SmartMenu.cs
@@ -10,12 +10,21 @@ namespace SmartMenuLibrary
     {
         public void LoadMenu(string path)
         {
+            int counter = 0;
+            string line;
+
             System.IO.StreamReader file =
-                new System.IO.StreamReader(@"..\..\MenuSpec.txt"); 
+                new System.IO.StreamReader(@"..\..\MenuSpec.txt");
+
+            while ((line = file.ReadLine()) != null)
+            {
+                System.Console.WriteLine(line);
+                counter++;
+            }
+            file.Close();
         }
         public void Activate()
         {
-            // Implement ...
 
         }
     }
